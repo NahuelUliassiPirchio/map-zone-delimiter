809c024c9d68c8d76178f1179bc86c0a9c5e3ad1
Nahuel Uliassi Pirchio <uliassipirchio@gmail.com>
2023-10-31 23:20:10 -0300

    [➕ Add] Basic sidebar & onclick marker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const points = []
+
+const groupInput = document.getElementById('group-input')
+
+function actualizarSidebar() {
+    const sidebar = document.getElementById('points-container');
+    sidebar.innerHTML = points.map(point => `<li>${point.group},${point.latlng.lat},${point.latlng.lng}</li>`).join('');
+}
 const map = window.L.map('map').setView([-34.626056, -58.496659], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,5 +14,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 map.on('click', event => {
+    console.log(event.latlng)
+    points.push({
+        latlng: event.latlng,
+        group: groupInput.value || 'group1'
+    })
     L.marker(event.latlng).addTo(map);
+    actualizarSidebar()
 })
